8c2fb5a0f0722e86e2e0c7931c95a09d81fada37
Samuel Rodriguez <samrodriguez826@gmail.com>
2023-02-28 11:14:01 -0500

    added fontFamily to config and started bio

--- react-portfolio/src/pages/index.js
+++ react-portfolio/src/pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main className="bg-white px-10">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
-            <h1 className="text-xl">developedbySR</h1>
+            <h1 className="text-xl font-burtons">developedbySR</h1>
             <ul className="flex items-center">
               <li>
                 <BsFillMoonStarsFill className="cursor-pointer text-2xl"/>
@@ -21,6 +21,13 @@ export default function Home() {
               <li><a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="#">Resume</a></li>
             </ul>
           </nav>
+          <div>
+            <h2>Sam Rodriguez</h2>
+            <h3>Scientist and artist.</h3>
+            <p>
+              
+            </p>
+          </div>
         </section>
       </main>
     </>
