9edf188e886b19f6563514e812e9e947bf4f791c
Samuel Rodriguez <samrodriguez826@gmail.com>
2023-02-28 21:37:37 -0500

    headshot and card divs

--- react-portfolio/src/pages/index.js
+++ react-portfolio/src/pages/index.js
@@ -6,7 +6,10 @@ import {
   AiFillYoutube
 } from 'react-icons/ai';
 import Image from "next/image";
-import deved from '../../public/dev-ed-wave.png';
+import daisySam from '../../public/srFilmDJ.jpg';
+import design from '../../public/design.png';
+import code from '../../public/code.png';
+import consulting from '../../public/consulting.png';
 
 export default function Home() {
   return (
@@ -40,10 +43,55 @@ export default function Home() {
             <AiFillLinkedin/>
             <AiFillYoutube/>
           </div>
-          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden">
-            <Image src={deved} layout='fill' objectFit='cover' />
+          <div className="relative mx-auto rounded-full w-80 h-80 mt-20 overflow-hidden">
+            <Image src={daisySam}   objectFit='cover' width={325} height={325} />
           </div>
         </section>
+
+        <section>
+          <div>
+            <h3 className="text-3xl py-1"> Services I offer</h3>
+            <p className="text-md py-2 leading-8 text-gray-600">
+              Lorem ipsum
+            </p>
+          </div>
+          <div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10">
+              <div className="flex justify-center">
+                <Image src={design} width={100} height={100}/>
+              </div>
+              <h3 className="text-lg font-medium pt-8 pb-2">Beautiful designs</h3>
+              <p className="py-2">Creating elegant designs...</p>
+              <h4 className="py-4 text-teal-600">Design tools I use</h4>
+              <p className="text-gray-800 py-1">Photoshop</p>
+              <p className="text-gray-800 py-1">Illustrator</p>
+              <p className="text-gray-800 py-1">Figma</p>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10">
+              <div className="flex justify-center">
+                <Image src={code} width={100} height={100}/>
+              </div>
+              <h3 className="text-lg font-medium pt-8 pb-2">Beautiful designs</h3>
+              <p className="py-2">Creating elegant designs...</p>
+              <h4 className="py-4 text-teal-600">Design tools I use</h4>
+              <p className="text-gray-800 py-1">Photoshop</p>
+              <p className="text-gray-800 py-1">Illustrator</p>
+              <p className="text-gray-800 py-1">Figma</p>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10">
+              <div className="flex justify-center">
+                <Image src={consulting} width={100} height={100}/>
+              </div>
+              <h3 className="text-lg font-medium pt-8 pb-2">Beautiful designs</h3>
+              <p className="py-2">Creating elegant designs...</p>
+              <h4 className="py-4 text-teal-600">Design tools I use</h4>
+              <p className="text-gray-800 py-1">Photoshop</p>
+              <p className="text-gray-800 py-1">Illustrator</p>
+              <p className="text-gray-800 py-1">Figma</p>
+            </div>
+          </div>
+        </section>
+
       </main>
     </>
   )
